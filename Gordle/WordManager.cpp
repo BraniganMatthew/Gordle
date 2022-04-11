9d70f31c377f95774eb32233afe269a452739534
Matthew Branigan <braniganmatthew@ufl.edu>
2022-04-10 22:30:23 -0400

    Added WordManager Class

--- Gordle/WordManager.cpp
+++ Gordle/WordManager.cpp
@@ -1,6 +1,38 @@
 #include "WordManager.h"
 
 #include <iostream>
+#include <fstream>
+
+WordManager::WordManager()
+{
+	initalizeMap();
+}
+
+WordManager::~WordManager()
+{
+	
+}
+
+void WordManager::initalizeMap()
+{
+	//If File in appData exists
+	getFileOffline();
+	//Else download full list from github
+	getFileOnline();
+	//Get File and start inserting valid words
+	
+	//Repeat for easy words
+}
+
+void WordManager::getFileOnline()
+{
+
+}
+
+void WordManager::getFileOffline()
+{
+
+}
 
 void WordManager::testFunction()
 {
--- Gordle/WordManager.h
+++ Gordle/WordManager.h
@@ -1,9 +1,17 @@
 #pragma once
+#include <unordered_map>
 class WordManager
 {
 private:
-
+	std::unordered_map<std::string, std::string> validList, solutionList;
+	void getFileOnline();
+	void getFileOffline();
+	void initalizeMap();
 public:
+	WordManager();
+	~WordManager();
 	void testFunction();
+	std::string getDailyWord();
+	std::string getRandomWord();
 };
 
